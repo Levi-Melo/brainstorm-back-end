e43d466b1d3696a3e743bf9d66e4953ac1045a9b
levi melo <levicontat@outlook.com>
2021-11-13 19:01:04 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import variables from "dotenv";
 variables.config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
 });
 import { router } from "./routes";
+import { Fail } from "./entities/Error";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
 
-// app.use((error: Fail, req: Request, res: Response, next: NextFunction) => {
-//   const resJson = {
-//     status: false,
-//     errors: error.message || "Internal Server Error",
-//   };
+app.use((error: Fail, req: Request, res: Response, next: NextFunction) => {
+  const resJson = {
+    status: false,
+    errors: error.message || "Internal Server Error",
+  };
 
-//   const statusCode = error.status ? error.status : 500;
+  const statusCode = error.status ? error.status : 500;
 
-//   return res.status(statusCode).json(resJson);
-// });
+  return res.status(statusCode).json(resJson);
+});
 
 export { app };
