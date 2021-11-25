a4eaf3ca52aaed1375978a08ed229fc2d144c138
levi melo <levicontat@outlook.com>
2021-11-25 11:28:46 -0300

    sign fix

--- src/services/userServices/signIn/SignInUserService.ts
+++ src/services/userServices/signIn/SignInUserService.ts
@@ -5,15 +5,17 @@ import { ISignInUserRequest, ISignInUserService } from "./ISignInUserService";
 import { sign } from "jsonwebtoken";
 
 import { Fail } from "../../../entities/Error";
+import { compare } from "bcrypt";
 
 class SignInUserService implements ISignInUserService {
   async execute({ email, password }: ISignInUserRequest) {
     const userRepository = getCustomRepository(UserRepository);
 
-    const user = await userRepository.findOne({
-      where: { email, password },
-    });
-    if (!user) throw new Fail(404, "Incorrect email or password");
+    const user = await userRepository.findOne({ email });
+
+    if (!user || !(await compare(password, user.password))) {
+      throw new Fail(401, "Email/Password incorrect");
+    }
     if (!user.active) {
       throw new Fail(401, "User is not active contact a administrator");
     }
